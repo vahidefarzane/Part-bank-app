c7d26b846ac6a12651acf8d9d8a3b58eecb95f1f
vahide <vahidefarzane@gmail.com>
2024-08-31 04:21:08 +0430

    feat: add upload card component to file uploader paga

--- src/components/view/upload-card.vue
+++ src/components/view/upload-card.vue
@@ -1,20 +1,67 @@
-<script setup></script>
+<script setup>
+import { ref,watch,onMounted } from 'vue';
+const props = defineProps({
+  caption: {
+    type: String,
+    required: true
+  },
+  altImage: {
+    type: String
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  initialImageUrl: {
+    type: String,
+    default: null
+  }
+})
+const imageUrl = ref(props.initialImageUrl)
+const emit = defineEmits(['image-uploaded'])
+
+
+const onFileChange = (event) => {
+  const file = event.target.files[0]
+  if (file) {
+    imageUrl.value = URL.createObjectURL(file)
+    emit('image-uploaded', { id: props.id, file })
+  }
+}
+watch(imageUrl, (newUrl) => {
+  if (newUrl) {
+    emit('image-uploaded', { id: props.id, url: newUrl })
+  }
+})
+</script>
 <template>
   <div class="upload-card">
-    <div class="upload-card__link-wrapper" v-if="true">
-    <div class="upload-card__img" v-if="false"></div>
+    <div v-if="imageUrl" class="upload-card__img">
+      <img :src="imageUrl" :alt="altImage" />
+    </div>
 
-      <img class="upload-card__icon" src="./../../assets/icons/uploader.svg" alt="upload" />
-
-      <div class="upload-card__link">
-        <span class="upload-card__link_light">تصویر را بکشید و اینجا رها کنید یا </span>
-        <span class="upload-card__link_bold">کلیک کنید . </span>
+    <div class="upload-card__body-wrapper" v-if="true">
+      <div class="upload-card__body">
+        <img class="upload-card__icon" src="./../../assets/icons/uploader.svg" alt="upload" />
+        <label :for="id" class="upload-card__label">
+          <span class="upload-card__label_light">تصویر را بکشید و اینجا رها کنید یا </span>
+          <span class="upload-card__label_bold">کلیک کنید . </span>
+        </label>
+        <input
+          class="upload-card__input"
+          type="file"
+          :id="id"
+          @change="onFileChange"
+          accept="image/*"
+        />
       </div>
     </div>
 
     <div class="upload-card__caption caption">
-      <span class="caption__text">تصویر روی کارت ملی</span>
-      <span class="caption__options"> </span>
+      <span class="caption__text">{{ label }}</span>
+      <span class="caption__options">
+        <img src="../../assets/icons/more-dark.svg" alt="" />
+      </span>
     </div>
   </div>
 </template>
@@ -27,20 +74,25 @@
   width: 20rem;
   height: 14.5rem;
 
-  &__link-wrapper {
-    @include flex-box(column, center, center, 0.5rem);
-    background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='12' ry='12' stroke='%23E2EDFFFF' stroke-width='2' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e");
+  &__body-wrapper {
+    @include flex-box(column, center, center);
+    background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='12' ry='12' stroke='%23E2EDFFFF' stroke-width='2' stroke-dasharray='6%2c 10' stroke-dashoffset='4' stroke-linecap='round'/%3e%3c/svg%3e");
+    height: 11.25rem;
+  }
+  &__img {
     height: 11.25rem;
   }
   &__icon {
     width: 4.25rem;
     height: 3.15881rem;
   }
-  &__link {
+  &__body {
+    @include flex-box(column, center, center, 0.5rem);
+  }
+  &__label {
     text-align: center;
     width: 10rem;
     font-size: 0.875rem;
-    line-height: normal;
     &_light {
       color: var(--secondary-200);
     }
@@ -49,10 +101,13 @@
       font-weight: 500;
     }
   }
+  &__input {
+    visibility: hidden;
+  }
 }
 .caption {
-  padding: 1rem;
-
+  @include flex-box(row, space-between, center);
+  padding: 1rem 0.75rem;
   &__text {
     @include font-style(0.875rem, 400);
     color: var(--black-500);
--- src/pages/file-uploader-page.vue
+++ src/pages/file-uploader-page.vue
@@ -4,8 +4,25 @@ import uploadCard from '@/components/view/upload-card.vue'
 </script>
 <template>
   <createDepositAccountWrapper title="تصویر کارت ملی">
-    <uploadCard />
+    <div class="uploader-container">
+      <uploadCard
+        caption="تصویر روی کارت ملی"
+        altImage="Front of National Card"
+        id="front"
+        @image-uploaded="handleImageUpload"
+      />
+      <uploadCard
+        caption="تصویر پشت کارت ملی"
+        altImage="back of National Card"
+        id="back"
+        @image-uploaded="handleImageUpload"
+      />
+    </div>
   </createDepositAccountWrapper>
 </template>
 
-<style></style>
+<style lang="scss">
+.uploader-container {
+  @include flex-box(row, center, center, 2.5rem);
+}
+</style>
