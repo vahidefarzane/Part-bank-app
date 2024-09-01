019e8c727008292945c6f2e03d56792dd23c097f
vahide <vahidefarzane@gmail.com>
2024-09-01 10:15:22 +0430

    feat: create dashboard layout

---
+++ src/layouts/dashboard-layout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import theSidebar from '@/components/view/the-sidebar.vue'
+</script>
+
+<template>
+  <main class="dashboard-layout">
+    <theSidebar />
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss">
+.dashboard-layout {
+  @include flex-box(row, center, null, 1rem);
+}
+</style>
