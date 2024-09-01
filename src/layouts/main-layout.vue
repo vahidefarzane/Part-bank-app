15673f1e0ea439f8661f81e7c695600430f68b76
vahide <vahidefarzane@gmail.com>
2024-09-01 10:00:44 +0430

    feat: create main layout

---
+++ src/layouts/main-layout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import theHeader from '@/components/view/the-header.vue'
+</script>
+
+<template>
+  <main class="main-layout">
+    <theHeader />
+    <RouterView />
+  </main>
+</template>
+
+<style lang="scss">
+.main-layout {
+  display: flex;
+  flex-direction: column;
+  background-color: var(--secondary-50);
+  height: 100vh;
+}
+</style>
