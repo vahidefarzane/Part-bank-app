332750686626ed4ed4c45a78de22fe6bf359205d
vahide <vahidefarzane@gmail.com>
2024-09-01 09:48:50 +0430

    style: add style for sidebar component

--- src/components/view/sidebar-item.vue
+++ src/components/view/sidebar-item.vue
@@ -17,10 +17,24 @@ defineProps({
 </script>
 
 <template>
-  <div class="side-bar-item">
-    <img :src="src" :alt="altImgText" />
-    <span>{{ title }}</span>
+  <div class="sidebar-item">
+    <img class="sidebar-item_icon" :src="src" :alt="altImgText" />
+    <span class="sidebar-item_name">{{ title }}</span>
   </div>
 </template>
 
-<style></style>
+<style lang="scss">
+.sidebar-item {
+  @include flex-box(row, flex-start, center, 0.5rem);
+  height: 2.75rem;
+  padding: 0.5rem 1rem 0.5rem 0.75rem;
+  border-radius: 0.375rem;
+  &:hover {
+    background-color: var(--secondary-300);
+  }
+  &_name {
+    @include font-style(0.875rem, 400);
+    color: var(--secondary-200);
+  }
+}
+</style>
--- src/components/view/the-sidebar.vue
+++ src/components/view/the-sidebar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import baseDivider from '../common/base-divider.vue'
+import sidebarItem from './sidebar-item.vue'
 import dashboardIcon from '@/assets/icons/element-4.svg'
 import walletIocn from '@/assets/icons/wallet.svg'
 import edit2 from '@/assets/icons/edit2.svg'
@@ -22,25 +24,25 @@ const sideBarListItems = reactive([
 ])
 </script>
 <template>
-  <aside class="side-bar">
-    <div class="side-bar__header">
-      <span class="side-bar__name">وحیده فرزانه</span>
-      <div class="side-bar__code">
-        <span class="side-bar__code_title">کدملی:</span>
-        <span class="side-bar__code_number">۰۹۱۲۳۴۵۶۷۸</span>
+  <aside class="sidebar">
+    <div class="sidebar__header">
+      <span class="sidebar__name">وحیده فرزانه</span>
+      <div class="sidebar__code">
+        <span class="sidebar__code_title">کدملی:</span>
+        <span class="sidebar__code_number">۰۹۱۲۳۴۵۶۷۸</span>
       </div>
     </div>
-    <dividerHr />
-    <ul class="side-bar__list">
-      <li class="" v-for="listItem in sideBarListItems" :key="listItem.id">
-        <sideBarItem
+    <baseDivider class="sidebar__divider" />
+    <ul class="sidebar__list">
+      <li v-for="listItem in sideBarListItems" :key="listItem.id">
+        <sidebarItem
           :src="listItem.src"
           :title="listItem.title"
           :altImgText="listItem.altImgText"
         />
       </li>
       <li>
-        <div class="side-bar__list_last" @click="handelLogout">
+        <div class="sidebar__list_last" @click="handelLogout">
           <img :src="exit" alt="exit" />
           <span>خروج</span>
         </div>
@@ -49,4 +51,39 @@ const sideBarListItems = reactive([
   </aside>
 </template>
 
-<style></style>
+<style lang="scss">
+.sidebar {
+  @include flex-box(column, null, center, 1.25rem);
+  width: 13.75rem;
+  height: 50.25rem;
+  padding: 1.5rem 0rem 0.5rem 0rem;
+  background-color: var(--color-white);
+  &__header {
+    @include flex-box(column, null, center, 2.25rem);
+  }
+  &__name {
+    @include font-style(1.125rem, 500);
+    color: var(--black-500);
+  }
+  &__code {
+    @include flex-box(column, space-between, center);
+    &_title {
+      color: var(--secondary-200);
+      @include font-style(0.75rem, 400);
+    }
+    &_number {
+      @include font-style(0.875rem, 400);
+    }
+  }
+  &__divider {
+    width: 12.25rem;
+  }
+  &__list {
+    @include flex-box(column, null, flex-start, 0.125rem);
+    padding: 0rem 0.5rem;
+    &_last{
+        color: var(--fail-500);
+    }
+  }
+}
+</style>
