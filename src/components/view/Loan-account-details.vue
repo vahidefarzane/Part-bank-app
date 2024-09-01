4e23934af968ad8f112ef0e282787cbd3059feb9
vahide <vahidefarzane@gmail.com>
2024-09-01 07:26:11 +0430

    feat: create loan account datails component

---
+++ src/components/view/Loan-account-details.vue
@@ -0,0 +1,48 @@
+<script setup>
+import BaseButton from '../common/base-button.vue'
+
+defineProps({
+  title: {
+    type: String,
+    Required: true
+  },
+  buttonText: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="loan-account">
+    <div class="loan-account__info-bar">
+      <h1 class="loan-account__title">{{ title }}</h1>
+      <slot name="title-info" />
+    </div>
+
+    <slot name="scores" />
+
+    <div class="loan-account__action-bar">
+      <BaseButton class="loan-account__action-button secondary">
+        {{ buttonText }}
+        <template #post-icon> <img src="" alt="" /></template>
+      </BaseButton>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.loan-account {
+  @include flex-box(column, space-between, center);
+  background-color: var(--color-white);
+  border-radius: 0.75rem;
+  height: 16.25rem;
+  padding: 1.5rem;
+  &__info-bar {
+    @include flex-box(row, space-between, center);
+  }
+  &__title {
+    color: var(--black-500);
+    @include font-style(1.25rem, 500);
+  }
+}
+</style>
