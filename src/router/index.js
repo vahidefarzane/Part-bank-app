901c87ae8f0df658457190f7e6e54050fa777c13
vahide <vahidefarzane@gmail.com>
2024-09-01 10:31:53 +0430

    feat: add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,79 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '@/pages/login-page.vue'
 import OtpPage from '@/pages/otp-page.vue'
-import CreateDepositAccountPage from '@/pages/create-deposit-account-page.vue'
-import reviewInfoPage from '@/pages/review-info-page.vue'
-import fileUploaderPage from '@/pages/file-uploader-page.vue'
+import dashboardLayout from '@/layouts/dashboard-layout.vue'
+import mainLayout from '@/layouts/main-layout.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: '/',
-      component: fileUploaderPage
-    },
-    {
-      path: '/review-info',
-      component: reviewInfoPage
-    },
-    {
-      path: '/create-deposit-account',
-      component: CreateDepositAccountPage
-    },
     {
       path: '/otp-page',
       component: OtpPage
     },
     {
       path: '/login',
-      component: LoginPage
+      name: 'Login',
+      component: LoginPage,
+      meta: {
+        title: 'پارت بانک | ورود',
+        requiresAuth: false
+      }
+    },
+    {
+      path: '/',
+      name: 'Account',
+      component: mainLayout,
+
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: dashboardLayout,
+          meta: {
+            title: 'پارت بانک | داشبورد',
+            requiresAuth: true
+          },
+          children: [
+            {
+              path: 'home',
+              name: 'Home',
+              meta: {
+                title: 'داشبورد | صفحه اصلی',
+                requiresAuth: true
+              },
+              component: () => import('@/pages/dashboard-page.vue')
+            }
+          ]
+        },
+        {
+          path: '/create-deposit-account',
+          name: 'CreateDepositAccountPage',
+          component: () => import('@/pages/create-deposit-account-page.vue'),
+          meta: {
+            title: 'پارت بانک | اطلاعات فردی',
+            requiresAuth: true
+          }
+        },
+        {
+          path: '/file-uploader',
+          name: 'FileUploader',
+          component: () => import('@/pages/file-uploader-page.vue'),
+          meta: {
+            title: 'پارت بانک | بارگزاری فایل',
+            requiresAuth: true
+          }
+        },
+        {
+          path: '/review-info',
+          name: 'ReviewInfo',
+          component: () => import('@/pages/review-info-page.vue'),
+          meta: {
+            title: 'پارت بانک | تایید اطلاعات',
+            requiresAuth: true
+          }
+        }
+      ]
     }
   ]
 })
