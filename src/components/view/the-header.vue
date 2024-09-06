d7bf3ec2f5e74ed5251b2125c0458e4cec25b063
vahide <vahidefarzane@gmail.com>
2024-09-06 05:55:44 +0430

    feat: add title for transaction list component

--- src/components/view/the-header.vue
+++ src/components/view/the-header.vue
@@ -1,23 +1,25 @@
 <script></script>
 
 <template>
-  <header class="header">
-    <section class="header__logo">
-      <img class="header__logo_icon" src="@/assets/icons/logo.svg" alt="logo" />
-      <span class="header__logo_caption">پارت بانک</span>
-    </section>
-    <section class="header__user-info user-info">
-      <img
-        class="user-info__notification"
-        src="@/assets/icons/Notification.svg"
-        alt="notification"
-      />
-      <div class="user-info__avatar avatar">
-        <img class="avatar__image" src="@/assets/icons/avatar.svg" alt="avatar" />
-        <span class="avatar__number">۰۹۲۲۶۵۹۸۷۲</span>
-      </div>
-    </section>
-  </header>
+  <div class="main-header">
+    <header class="header">
+      <section class="header__logo">
+        <img class="header__logo_icon" src="@/assets/icons/logo.svg" alt="logo" />
+        <span class="header__logo_caption">پارت بانک</span>
+      </section>
+      <section class="header__user-info user-info">
+        <img
+          class="user-info__notification"
+          src="@/assets/icons/Notification.svg"
+          alt="notification"
+        />
+        <div class="user-info__avatar avatar">
+          <img class="avatar__image" src="@/assets/icons/avatar.svg" alt="avatar" />
+          <span class="avatar__number">۰۹۲۲۶۵۹۸۷۲</span>
+        </div>
+      </section>
+    </header>
+  </div>
 </template>
 
 <style lang="scss">
--- src/components/view/transaction-list.vue
+++ src/components/view/transaction-list.vue
@@ -1,10 +1,36 @@
 <script setup>
-import dashboardTable from './dashboard-table.vue';
+import dashboardTable from './dashboard-table.vue'
 </script>
 <template>
-  <main>
-    <dashboardTable/>
+  <main class="transaction">
+    <section class="transaction__header header">
+      <div class="header__title">
+        <span class="header__title_name">لیست تراکنش‌ها</span>
+        <span class="header__title_unit"> ( ریال )</span>
+      </div>
+      <div class="transaction__options">search</div>
+    </section>
+    <dashboardTable />
   </main>
 </template>
 
-<style></style>
+<style lang="scss">
+.transaction {
+  @include flex-box(column, null, null, 1.5rem);
+  &__header {
+    @include flex-box(row, space-between, center);
+  }
+  .header {
+    &__title {
+      &_name {
+        @include font-style(1.25rem, 700);
+        color: var(--black-500);
+      }
+      &_unit {
+        @include font-style(1rem, 400);
+        color: var(--secondary-200);
+      }
+    }
+  }
+}
+</style>
